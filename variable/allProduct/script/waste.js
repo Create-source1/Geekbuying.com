356094a06e00a1fe017ded0dfca9b7cf59b44340
Create-source1 <jaiswalpj13@gmail.com>
2025-05-05 12:40:44 +0530

    changes

--- variable/allProduct/script/waste.js
+++ variable/allProduct/script/waste.js
@@ -34,3 +34,55 @@
         //     </div>
         //   </div>
         // </div>
+
+        let {useState} = React;
+
+        function ProductCards(){
+                let [products, setProduct] = useState([])
+                let[count, setCount] = useState(0)
+
+                if(products === null){
+                        axios.get('https://geekbuying-8e592-default-rtdb.asia-southeast1.firebasedatabase.app/productData.json')
+                        .then(response =>{
+                                let responseArray = Object.entries(response.data).map(([id, data])=> ({id, ...data, likes:0}))
+                                setProduct(responseArray)
+                                // console.log(responseArray)
+                        })
+                        .catch(error=>{
+                                console.log('Error:', error.message)
+                        })        
+                }
+
+                function handleClick(id){
+                        setCount(prev => prev + 1)
+                }
+                return(
+                        <>
+                        <div className='product-card'>
+                        <h2>Product List</h2>
+                        <div className='product-list'>
+                        {products ? (
+                                products.map(product=>(
+                                        <div key={product.id}>
+                                        <h3>Name: {product.name}</h3> 
+                                        <image src={product.image} alt={product.name}/>
+                                        <p>Price: {product.price}</p>
+                                        <button onClick={()=> handleClick(product.id)}>Like ❤️</button>
+                                        </div>     
+                         ))
+                        )  : (<p>Loding Products...</p>) }
+                        
+                        </div>
+                        </div>
+                        </>
+                )
+        }
+
+        function App(){
+                return(
+                        <>
+                        <ProductCards/>
+                        </>
+                )
+        }
+        ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
